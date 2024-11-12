e9e3d51588b79f8efcca386c3a500975859a3119
CrabtreeAJ <a.crabtree@student.maastrichtuniversity.nl>
2024-11-12 12:33:51 +0100

    Person Repo

---
+++ src/main/java/lsit/Repositories/PersonRepository.java
@@ -0,0 +1,36 @@
+package lsit.Repositories;
+
+import java.util.*;
+
+import org.springframework.stereotype.Repository;
+
+import lsit.Models.Person;
+
+@Repository
+public class PersonRepository {
+
+    static HashMap<UUID, Person> person = new HashMap<>();
+
+    public void add(Person p){
+        p.id = UUID.randomUUID();
+        person.put(p.id, p);
+    }
+
+    public Person get(UUID id){
+        return person.get(id);
+    }
+
+    public void remove(UUID id){
+        person.remove(id);
+    }
+
+    public void update(Person p){
+        Person x = person.get(p.id);
+        x.role = p.role;
+    }
+
+    public List<Person> list(){
+        return new ArrayList<>(person.values());
+    }
+    
+}
