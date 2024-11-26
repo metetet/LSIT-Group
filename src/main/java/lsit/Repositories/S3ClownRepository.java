236372f427bcd981f0c33433f2b2b42c86e9da2a
Mete Tugay <115257080+metetet@users.noreply.github.com>
2024-11-26 15:35:16 +0100

    s3 clown

---
+++ src/main/java/lsit/Repositories/S3ClownRepository.java
@@ -0,0 +1,126 @@
+package lsit.Repositories;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Repository;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Object;
+
+import lsit.Models.Clown;
+
+@Repository
+public class S3ClownRepository implements IntClownRepository{
+    final String BUCKET="lsit-example-bucket";
+    final String PREFIX="cc.inc/clowns/";
+    final String ACCESS_KEY="GOOG1EM4BODFHSWVPEFXA3FWS3TEG6CIL5RZ7WHQ3QN66MMUF5VKVAPAXYZCH";
+    final String SECRET_KEY="v/8XBIN27gtNjdRcK5ffIUcioMVyJur5UG7hPy24";
+    final String ENDPOINT_URL="https://storage.googleapis.com";
+
+    S3Client s3client;
+    AwsCredentials awsCredentials;
+
+    public S3ClownRepository() {
+        awsCredentials = AwsBasicCredentials.create(ACCESS_KEY, SECRET_KEY);
+        s3client = S3Client.builder()
+            .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+            .endpointOverride(URI.create(ENDPOINT_URL))
+            .region(Region.of("auto"))
+            .build();
+    }
+
+    public void add(Clown p){
+        try{
+            p.id = UUID.randomUUID();
+
+            ObjectMapper om = new ObjectMapper();
+
+            String clownJson = om.writeValueAsString(p);
+            
+            s3client.putObject(PutObjectRequest.builder()
+                .bucket(BUCKET)
+                .key(PREFIX + p.id.toString())
+                .build(),
+                RequestBody.fromString(clownJson)
+            );
+        }
+        catch(JsonProcessingException e){}
+    }
+
+    public Clown get(UUID id){
+        try{
+            var objectBytes = s3client.getObject(GetObjectRequest.builder()
+                .bucket(BUCKET)
+                .key(PREFIX + id.toString())
+                .build()
+            ).readAllBytes();
+
+            ObjectMapper om = new ObjectMapper();
+            Clown p = om.readValue(objectBytes, Clown.class);
+
+            return p;
+        }catch(Exception e){
+            return null;
+        }
+    }
+
+    public void remove(UUID id){
+        s3client.deleteObject(DeleteObjectRequest.builder()
+            .bucket(BUCKET)
+            .key(PREFIX + id.toString())
+            .build()
+        );  
+    }
+
+    public void update(Clown p){
+        try{
+            Clown x = this.get(p.id);
+            if(x == null) return;
+
+            ObjectMapper om = new ObjectMapper();
+            String clownJson = om.writeValueAsString(p);
+            s3client.putObject(PutObjectRequest.builder()
+                .bucket(BUCKET)
+                .key(PREFIX + p.id.toString())
+                .build(),
+                RequestBody.fromString(clownJson)
+            );
+        }
+        catch(JsonProcessingException e){}
+    }
+
+
+    public List<Clown> list(){
+        List<Clown> clowns = new ArrayList<Clown>();
+        List<S3Object> objects = s3client.listObjects(ListObjectsRequest.builder()
+          .bucket(BUCKET)
+          .prefix(PREFIX)
+          .build()  
+        ).contents();
+
+        for(S3Object o : objects){
+            Clown p = new Clown();
+            //p = this.get(UUID.fromString(o.key().substring(PREFIX.length())));
+            p.id = UUID.fromString(o.key().substring(PREFIX.length()));
+            clowns.add(p);
+        }
+
+        return clowns;
+    }
+}
